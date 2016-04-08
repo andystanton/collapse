160022c5670f0ce189340395969c4568def689fd
Andy Stanton <andystanton@gmail.com>
2016-04-08 09:06:30 +0100

    Factor adding DOM element pixels out to collapse.js

--- src/collapse.js
+++ src/collapse.js
@@ -1,9 +1,9 @@
-var disassemble = function(element, first) {
-    function getPosition(element) {
-        var leftPos = $(element)[0].getBoundingClientRect().left + $(window)['scrollLeft']();
-        var rightPos = $(element)[0].getBoundingClientRect().right + $(window)['scrollLeft']();
-        var topPos = $(element)[0].getBoundingClientRect().top + $(window)['scrollTop']();
-        var bottomPos = $(element)[0].getBoundingClientRect().bottom + $(window)['scrollTop']();
+const disassemble = element => {
+    const getPosition = element => {
+        const leftPos = $(element)[0].getBoundingClientRect().left + $(window)['scrollLeft']();
+        const rightPos = $(element)[0].getBoundingClientRect().right + $(window)['scrollLeft']();
+        const topPos = $(element)[0].getBoundingClientRect().top + $(window)['scrollTop']();
+        const bottomPos = $(element)[0].getBoundingClientRect().bottom + $(window)['scrollTop']();
         return {
             left: leftPos,
             right: rightPos,
@@ -12,37 +12,53 @@ var disassemble = function(element, first) {
         };
     }
 
-    var childPromises = [];
+    const childPromises = [];
     if ($(element).find('.breakable').length) {
-        $(element).children().each(function(i, child) {
+        $(element).children().each((_, child) => {
             childPromises.push(disassemble(child));
         });
     }
 
-    return Promise.all(childPromises).then(function(images) {
-        var returnImages = [];
-        for (var i = 0; i < images.length; ++i) {
-            returnImages = returnImages.concat(images[i]);
-        }
+    return Promise.all(childPromises)
+        .then(images => {
+            var returnImages = [];
+            for (let i = 0; i < images.length; ++i) {
+                returnImages = returnImages.concat(images[i]);
+            }
 
-        if ($(element).hasClass("breakable")) {
-            var position = getPosition(element);
+            if ($(element).hasClass("breakable")) {
+                const position = getPosition(element);
 
-            return domtoimage.toSvg($(element)[0]).then(function(dataUrl) {
-                var img = new Image();
-                img.src = dataUrl;
+                return domtoimage.toSvg($(element)[0]).then(dataUrl => {
+                    const img = new Image();
+                    img.src = dataUrl;
 
-                var outImages = [{
-                    data: img,
-                    position: getPosition($(element))
-                }];
+                    const outImages = [{
+                        data: img,
+                        position: getPosition($(element))
+                    }];
 
-                $(element).css('visibility', 'hidden');
+                    $(element).css('visibility', 'hidden');
 
-                return outImages.concat(returnImages);
-            })
-        } else {
-            return Promise.resolve(returnImages);
-        }
-    });
+                    return outImages.concat(returnImages);
+                })
+            } else {
+                return Promise.resolve(returnImages);
+            }
+        });
 };
+
+const addPixels = images => images.map(imageWrapper => {
+    const image = imageWrapper.data;
+    const svgCanvas = $('<canvas>');
+
+    const ctx = svgCanvas[0].getContext('2d');
+    ctx.drawImage(image, 0, 0, image.width, image.height);
+
+    const pixelData = ctx.getImageData(0, 0, image.width, image.height).data;
+    imageWrapper.pixels = pixelData;
+
+    svgCanvas.remove();
+
+    return imageWrapper;
+});
