626098dc429e10ee0ecd05606644e91d7e5399a0
andystanton <andystanton@gmail.com>
2016-04-10 21:39:17 +0100

    Elements can collapse into chunks > 1

--- src/collapse.js
+++ src/collapse.js
@@ -80,29 +80,61 @@ const setupScene = images => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.position.z = 5;
 
-    const rectLength = 1;
-    const rectWidth = 1;
-    const rectShape = new THREE.Shape();
-
-    rectShape.moveTo(0, 0);
-    rectShape.lineTo(0, rectWidth);
-    rectShape.lineTo(rectLength, rectWidth);
-    rectShape.lineTo(rectLength, 0);
-    rectShape.lineTo(0, 0);
-
-    const rectGeom = new THREE.ShapeGeometry(rectShape);
+    const chunkSize = 2;
 
     const materials = {};
+    const geoms = {};
 
-    const getMaterial = (r, g, b, a, o) => {
+    const assignUVs = geometry => {
+        geometry.computeBoundingBox();
+
+        const max = geometry.boundingBox.max;
+        const min = geometry.boundingBox.min;
+
+        const offset = new THREE.Vector2(0 - min.x, 0 - min.y);
+        const range = new THREE.Vector2(max.x - min.x, max.y - min.y);
+
+        geometry.faceVertexUvs[0] = [];
+        const faces = geometry.faces;
+
+        for (let i = 0; i < geometry.faces.length; i++) {
+            const v1 = geometry.vertices[faces[i].a];
+            const v2 = geometry.vertices[faces[i].b];
+            const v3 = geometry.vertices[faces[i].c];
+
+            geometry.faceVertexUvs[0].push([
+                new THREE.Vector2((v1.x + offset.x) / range.x, (v1.y + offset.y) / range.y),
+                new THREE.Vector2((v2.x + offset.x) / range.x, (v2.y + offset.y) / range.y),
+                new THREE.Vector2((v3.x + offset.x) / range.x, (v3.y + offset.y) / range.y)
+            ]);
+        }
+        geometry.uvsNeedUpdate = true;
+    };
+
+    const getGeom = (w, h) => {
+        const geomName = `${w}x${h}`;
+        if (!geoms[geomName]) {
+            const rectShape = new THREE.Shape();
+            rectShape.moveTo(0, 0);
+            rectShape.lineTo(0, w);
+            rectShape.lineTo(h, w);
+            rectShape.lineTo(h, 0);
+            rectShape.lineTo(0, 0);
+            geoms[geomName] = new THREE.ShapeGeometry(rectShape);
+            assignUVs(geoms[geomName]);
+        }
+        return geoms[geomName];
+    };
+
+    const getMaterial = (r, g, b, a) => {
         const rgbName = `${r},${g},${b}`;
         const rgbaName = `${rgbName},${a}`;
         if (!materials[rgbaName]) {
-            var texture = new THREE.DataTexture(Uint8Array.from([r,g,b,a]), 1, 1, THREE.RGBAFormat, THREE.UnsignedByteType);
+            var texture = new THREE.DataTexture(Uint8Array.from([r, g, b, a]), 1, 1, THREE.RGBAFormat);
             texture.needsUpdate = true;
             materials[rgbaName] = new THREE.MeshBasicMaterial({
-              'map': texture,
-              'transparent': o > 0
+                'map': texture,
+                'transparent': a > 0
             });
         }
         return materials[rgbaName];
@@ -112,25 +144,80 @@ const setupScene = images => {
 
     images.forEach(imageWrapper => {
         const image = imageWrapper.data;
-        for (let x = 0; x < imageWrapper.width; ++x) {
-            for (let y = 0; y < imageWrapper.height; ++y) {
-                const pixelOffset = (x * 4) + (y * imageWrapper.width * 4);
-                const opacity = 1.0 / 255 * imageWrapper.pixels[pixelOffset + 3];
-                if (opacity > 0) {
-                    const mesh = new THREE.Mesh(
-                        rectGeom,
-                        getMaterial(
-                            imageWrapper.pixels[pixelOffset + 0],
-                            imageWrapper.pixels[pixelOffset + 1],
-                            imageWrapper.pixels[pixelOffset + 2],
-                            imageWrapper.pixels[pixelOffset + 3],
-                            opacity));
-                    mesh.position.x = imageWrapper.position.left + x;
-                    mesh.position.y = window.innerHeight - imageWrapper.position.top - y;
-                    scene.add(mesh);
-                    meshes[mesh.id] = {
-                        direction: new THREE.Vector2(0, 0)
-                    };
+        for (let y = 0; y < imageWrapper.height; y += chunkSize) {
+            for (let x = 0; x < imageWrapper.width; x += chunkSize) {
+                if (chunkSize == 1) {
+                    const pixelOffset = (x * 4) + (y * imageWrapper.width * 4);
+                    if (imageWrapper.pixels[pixelOffset + 3] > 0) {
+                        const mesh = new THREE.Mesh(
+                            getGeom(chunkSize, chunkSize),
+                            getMaterial(
+                                imageWrapper.pixels[pixelOffset + 0],
+                                imageWrapper.pixels[pixelOffset + 1],
+                                imageWrapper.pixels[pixelOffset + 2],
+                                imageWrapper.pixels[pixelOffset + 3]));
+                        mesh.position.x = imageWrapper.position.left + x;
+                        mesh.position.y = window.innerHeight - imageWrapper.position.top - y;
+                        scene.add(mesh);
+                        meshes[mesh.id] = {
+                            direction: new THREE.Vector2(0, 0)
+                        };
+                    }
+                } else {
+                    var transparentCount = 0;
+                    var invisibleCount = 0;
+                    var pixels = [];
+
+                    for (let iy = chunkSize; iy >= 0; --iy) {
+                        if (y + iy < imageWrapper.height) {
+                            for (let ix = 0; ix < chunkSize; ++ix) {
+                                const pixelOffset = ((x + ix) * 4) + ((y + iy) * imageWrapper.width * 4);
+                                if (x + ix < imageWrapper.width) {
+                                    const r = imageWrapper.pixels[pixelOffset + 0];
+                                    const g = imageWrapper.pixels[pixelOffset + 1];
+                                    const b = imageWrapper.pixels[pixelOffset + 2];
+                                    const a = imageWrapper.pixels[pixelOffset + 3];
+                                    if (a < 255) {
+                                        transparentCount++;
+                                        if (a == 0) {
+                                            invisibleCount++;
+                                        }
+                                    }
+                                    pixels.push(r, g, b, a);
+                                } else {
+                                    pixels.push(0, 0, 0, 0);
+                                    transparentCount++;
+                                    invisibleCount++;
+                                }
+                            }
+                        } else {
+                            for (let ix = 0; ix < chunkSize; ++ix) {
+                                pixels.push(0, 0, 0, 0);
+                                transparentCount++;
+                                invisibleCount++;
+                            }
+                        }
+                    }
+                    if (invisibleCount < (chunkSize * chunkSize)) {
+                        const texture = new THREE.DataTexture(
+                            Uint8Array.from(pixels),
+                            chunkSize,
+                            chunkSize,
+                            THREE.RGBAFormat);
+                        texture.needsUpdate = true;
+                        const mesh = new THREE.Mesh(
+                            getGeom(chunkSize, chunkSize),
+                            new THREE.MeshBasicMaterial({
+                                'map': texture,
+                                'transparent': transparentCount > 0
+                            }));
+                        mesh.position.x = imageWrapper.position.left + x;
+                        mesh.position.y = window.innerHeight - imageWrapper.position.top - y;
+                        scene.add(mesh);
+                        meshes[mesh.id] = {
+                            direction: new THREE.Vector2(0, 0)
+                        };
+                    }
                 }
             }
         }
